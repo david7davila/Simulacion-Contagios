63566e8428a957f770705ded362b6d0baa5946c7
GerardodrareG <65517190+GerardodrareG@users.noreply.github.com>
2020-05-17 20:09:01 -0500

    Cambio 051720 2008

--- 051720.R
+++ 051720.R
@@ -1,10 +1,22 @@
 
-    print("Holoa")
-    m_estados=matrix(NA,nrow=pob,ncol=length(estados))
-    for(i in 1:dim(m_estados)[1]){
-      estados=rep(0,8)
-      pos=sample(c(3:8),1)
+
+     #Matriz de estados
+     #1.- 
+    pob=10000
+    no_estados=4
+    m_estados=matrix(NA,nrow=pob,ncol=no_estados)
+    for(i in 1:pob){
+      estados=rep(0,4)
+      pos=sample(c(1:3),1)
       estados[pos]=1
       m_estados[i,]=estados
     }
     m_estados
+    
+    
+
+    AAA<-c("A","B","C","D")
+    for(i in AAA){
+      print(i)
+
+    }  
