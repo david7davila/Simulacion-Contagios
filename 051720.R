adcaa4da21baa9cf8c0d790cd0fb91e5d1c0276f
GerardodrareG <65517190+GerardodrareG@users.noreply.github.com>
2020-05-17 20:02:48 -0500

    V1
    051720

--- 051720.R
+++ 051720.R
@@ -1,3 +1,10 @@
 
     print("Holoa")
-    
+    m_estados=matrix(NA,nrow=pob,ncol=length(estados))
+    for(i in 1:dim(m_estados)[1]){
+      estados=rep(0,8)
+      pos=sample(c(3:8),1)
+      estados[pos]=1
+      m_estados[i,]=estados
+    }
+    m_estados
