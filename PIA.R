0e0a34dca029f6a04a95b2fb16fcb868e8133e8d
David7Davila <david7davila@gmail.com>
2020-06-05 00:50:01 -0500

    PIAA

--- PIA.R
+++ PIA.R
@@ -1,7 +1,7 @@
 #Columna 1: Identificador del individuo (nï¿½mero:ID)
 #Columna 2: Posicion dentro del Tablero (fila)
 #Columna 3: Posicion dentro del Tablero (columna)
-#Columna 4: Estado del individuo [0:susceptible, 1:infectado, 2:vacunado, 3:recuperado, 4:latente, 5:muerto, 6:cuarentena]
+#Columna 4: Estado del individuo [0:susceptible, 1:infectado, 2:vacunado, 3:recuperado, 4:latente, 5:muerto]
 #Columna 5: Edad [aï¿½os]
 #Columna 6: Tasa de contagio [probabilidad]
 #Columna 7: Tasa de letalidad [probabilidad]
@@ -13,24 +13,31 @@
 #Columna 13: TipoTraslado [0:CochePropio, 1:Transporte publico]
 #Columna 14: TipodeTrabajo [0:Notrabja, 1:BajoRiesgo,2:MedioRiesgo,3:AltoRiesgo]
 #Columna 15: Tasa de recuperación [probabilidad]
+#Columna 16: Cuarentena [binaria]
+#Columna 17: Tiempo tras vacunación
+#Columna 18: Tiempo recuperado
+#Columna 19: Lugar de trabjo coordenada X
+#Columna 20: Lugar de trabjo coordenada Y
 #--------------------------------------------------------------------------------------------
-Pob<-100
+library(random)
+
+Pob<-10000
 Dias<-100
+
 #For principal
 for(i in 1:Dias){ #Comienza el conteo en dias
   if(i==1){
+
+    Poblacion<-matrix(NA,nrow=Pob,ncol=20)
     
-    Mapa<-matrix(1,nrow=sqrt(Pob),ncol=sqrt(Pob)) 
-    Poblacion<-matrix(NA,nrow=Pob,ncol=15)
-    
-    #0-Susceptible, 1-Infectado, 2-vacunado, 3-Recuperado, 4:Latente, 5:muerto, 6-Cuarentena
+    #0-Susceptible, 1-Infectado, 2-vacunado, 3-Recuperado, 4:Latente, 5:muerto
     Poblacion[,1]<-c(1:Pob) #Asignamos los id
     Poblacion[,4]<-rep(0) #Todos inician siendo Susceptible
     Poblacion[,11]<-rep(15) #Todos inician con total libertad
     Poblacion[,5]<-sample(0:100,size=Pob,TRUE) #Asigno edades aleatorias
     Poblacion[,12]<-rep(0) #Asigno que no trabajan a toda la poblacion
     Poblacion[,13]<-rep(1) #Asigno usan transporte publico a toda la poblacion
-    Poblacion[,14]<-rep(0)
+    Poblacion[,14]<-rep(0) #Asigno No trabaja
     Poblacion[,6]<-rep(.5) #Todos inician con una Tasa Contagio
     
     L<-length(Poblacion[,5][Poblacion[,5]>=18 & Poblacion[,5]<=65])
@@ -39,19 +46,28 @@ for(i in 1:Dias){ #Comienza el conteo en dias
     Poblacion[,13][Poblacion[,5]>=18 & Poblacion[,5]<=85]<-sample(c(0,1),size=LC,TRUE,prob=c(.4,.6)) #De los mayores de 18 que conducen
     LT<-length(Poblacion[,12][Poblacion[,12]==1])
     Poblacion[,14][Poblacion[,12]==1]<-sample(c(1,2,3),size=LT,TRUE,prob=c(0.3,0.6,0.1)) #De los que trabajan que tipo de riesgo
+    
+    Coordenadas<-Radio(LT) # Numero de aleatorios dentro del centro
+    length(Coordenadas[,1])
+   length(Poblacion[,19][Poblacion[,12]==1])
+    Poblacion[,19][Poblacion[,12]==1]<-Coordenadas[,1]
+    Poblacion[,20][Poblacion[,12]==1]<-Coordenadas[,2]
+    
     PA<-sample(1:Pob,1)
     PX<-sample(1:sqrt(Pob),1)
     PY<-sample(1:sqrt(Pob),1)
     Poblacion[PA,2]<-PY #Coordenada en Y o Fila
     Poblacion[PA,3]<-PX #Coordenada en X o Columna
     Poblacion[PA,4]<-1 #Generamos el primer infectado del modelo
+    
+    
   }
   Poblacion<-MedidasPrecaucion(i,Poblacion)
 }
 
 
 #-----------------FUNCIONES ----------------------------------------------------------
-#Hospital capacity 
+
 MedidasPrecaucion=function(i,Poblacion){
   FASE1<-20   # Numero de infectados para activar la fase
   FASE2<-200  # Numero de infectados para activar la fase
@@ -84,7 +100,6 @@ MedidasPrecaucion=function(i,Poblacion){
   Poblacion[,6][Poblacion[,12]==1 & Poblacion[,14]==2]<-Poblacion[,6][Poblacion[,12]==1 & Poblacion[,14]==2]*T_MedioR
   Poblacion[,6][Poblacion[,12]==1 & Poblacion[,14]==3]<-Poblacion[,6][Poblacion[,12]==1 & Poblacion[,14]==3]*T_AltoR
   }
-
   if(Infectados==FASE1){
     sum(Poblacion[,12])
     Trabaja<-which(Poblacion[,12]==1)
@@ -147,4 +162,36 @@ MedidasPrecaucion=function(i,Poblacion){
   }
   return(Poblacion)
 }
+
+Radio=function(N){
+Ciudad<-(sqrt(Pob))/2 # Se estable la mitad del mapa como el centro de la ciudad 
+
+RadiosCiudad<-25 #Simulando el tamaño de una ciudad
+P=0
+R=RadiosCiudad
+h<-Ciudad
+k<-Ciudad
+PuntoX<-NULL
+PuntoY<-NULL
+Dentro<-NULL
+while (P<N) {
+  Punto<- c(sample((Ciudad-R):(Ciudad+R),1),sample((Ciudad-R):(Ciudad+R),1))
+  if (((Punto[1]-h)^2 + (Punto[2]-k)^2)<R^2)
+  {
+    Dentro<-Dentro + 1
+    PuntoX<-c(PuntoX,Punto[1]) 
+    PuntoY<-c(PuntoY,Punto[2])
+    P=P+1
+  }
+}
+Coorde<-matrix(NA,N,2)
+Coorde[,1]<-PuntoX
+Coorde[,2]<-PuntoY
+plot(PuntoX,PuntoY)
+
+return(Coorde)
+}
+
+
+
 
